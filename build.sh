8c8c735c03d2c36b3922c5358740130dc77ef934
xb <ssbandjl@163.com>
2023-09-19 17:41:50 +0800

    mercury

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
  # @Author: xb ssbandjl@163.com
  # @Date: 2023-09-18 00:08:08
  # @LastEditors: xb ssbandjl@163.com
- # @LastEditTime: 2023-09-18 22:01:18
+ # @LastEditTime: 2023-09-19 17:41:49
  # @FilePath: /libfabric/build.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
@@ -21,6 +21,7 @@ clear
 # build_path=/home/xb/project/libfabric/libfabric/build
 # build_bin=/home/xb/project/libfabric/libfabric/build/bin
 # ./configure --with-libfabric=/home/xb/project/libfabric/libfabric/build/libfabric --prefix=/home/xb/project/libfabric/libfabric/build/ --enable-debug && make -j 32 && make install
+# for mercury ./configure --enable-debug && make -j 32 && make install
 ./configure --prefix=/home/xb/project/libfabric/libfabric/build/ --enable-debug && make -j 32 && make install
 
 # 测试安装成功, 可能缺少原子库:yum install libatomic* -y
