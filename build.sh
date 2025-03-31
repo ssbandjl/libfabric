5d2d3ecd49eda5686174081a64c8b5827abf0473
xb <bin@chinachina.cn>
2025-03-31 10:38:07 +0800

    update build.sh and c/s scripts
    Signed-off-by: xb <bin@chinachina.cn>

--- build.sh
+++ build.sh
@@ -30,3 +30,18 @@ export PATH=/root/project/net/libfabric//build/bin:$PATH
 echo "fi_info"
 
 
+# config on hpc_117
+./autogen.sh
+mkdir build
+./configure --enable-debug --with-libfabric=/root/project/net/libfabric/build/lib --prefix=/root/project/net/libfabric/build/
+make -j 64
+make install
+
+
+build fabtests:
+cd fabtests
+./autogen.sh
+./configure --enable-debug --with-libfabric=/root/project/net/libfabric/build/ --prefix=/root/project/net/libfabric/build/
+make -j64
+make install
+
